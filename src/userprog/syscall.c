45aeec2978052566d8599744513635e25b45ceb2
JEANMARIE MENYEMENYE <mjeanm8@hornbeam.rlogin>
2013-02-18 23:07:44 -0500

    memory access restrictions

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,8 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+
 
 static void syscall_handler (struct intr_frame *);
 
@@ -19,6 +21,10 @@ static int sys_write (int fd, const void *buffer, unsigned size);
 static void sys_seek (int fd, unsigned position);
 static unsigned int sys_tell (int fd);
 static void sys_close (int fd);
+
+static int get_user(const uint8_t *uaddr);
+static bool put_user(uint8_t *udst, uint8_t byte);
+static bool is_pointer_valid(const uint8_t *uaddr, bool read);
   
 void
 syscall_init (void) 
@@ -152,3 +158,56 @@ static void sys_close (int fd UNUSED)
 {
   return;
 }
+
+/* Read a byte at user virtual address UADDR.
+	 UADDR must be below PHYS_BASE.
+   Returns the byte value if successful, -1 if a 
+   segfault occured. */
+
+static int get_user(const uint8_t *uaddr)
+{
+	int result;
+	asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+ 	return result;
+}
+
+/* Writes BYTE to user address UDST. UDST must be below
+ *    PHYS_BASE. Returns true if successful, false if a
+ *       segfault occured. */
+static bool put_user(uint8_t *udst, uint8_t byte)
+{
+	int error_code;
+	  asm ("movl $1f, %0; movb %b2, %1; 1:"
+	       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+	  return error_code != -1;
+}
+
+/* Call this function only to figure out if you can
+ * read or write to an address provided by the user.
+ *
+ * - The caller of this function needs to figure out 
+ *   access/mode restrictions by himself.
+ *
+ * - Also if the pointer is not valid, the caller needs
+ * to do the necessary clean up ( free resources and 
+ * terminate the thread that passed the invalid pointer).
+ *
+ * uaddr: is the pointer you want to validate.
+ * read: is a flag indicating whether you want to read from
+ * or write to uaddr memory address.
+ *
+ */
+bool is_pointer_valid(const uint8_t *uaddr, bool read)
+{
+	if(( uaddr == NULL) || ( uaddr > PHYS_BASE ))
+	{
+		return false;
+	}
+	
+	if( read )
+		return get_user( uaddr ) != -1; 
+	else
+		return put_user( uaddr, 'm' );
+}
+
